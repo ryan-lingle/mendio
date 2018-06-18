ec7f6a2e87a83fd88192e3ad61fe5d32134d7205
Ryan Lingle <lingleryan@gmail.com>
2018-06-18 19:43:55 +0200

    work to be done

--- app/javascript/packs/podcast.js
+++ app/javascript/packs/podcast.js
@@ -4,6 +4,7 @@ function executePodcastSearch() {
   if (podcastInput) {
     focuser(podcastInput);
     clicker(podcastResults);
+    killEnter();
   }
 
 
@@ -28,22 +29,27 @@ function executePodcastSearch() {
   }
 
   const podcastKey = function (event) {
-      if (event.key == 'Enter') {
-        document.removeEventListener('keyup', podcastKey)
-      } else {
-        const query = podcastInput.value;
-        fetch(`https://itunes.apple.com/search?term=${query}&media=podcast&entity=podcast&limit=10`)
-          .then(response => response.json())
-          .then((data) => {
-            document.querySelector(".podcast-results").innerHTML = "";
-            if (data.results) {
-              data.results.reverse().forEach((podcast) => {
-                document.querySelector('.podcast-results').insertAdjacentHTML("afterbegin", `<div class="podcast"><img src="${podcast.artworkUrl60}" alt="d"><li>${podcast.collectionName}</li></div>`);
-              });
-            };
+    const query = podcastInput.value;
+    fetch(`https://itunes.apple.com/search?term=${query}&media=podcast&entity=podcast&limit=10`)
+      .then(response => response.json())
+      .then((data) => {
+        document.querySelector(".podcast-results").innerHTML = "";
+        if (data.results) {
+          data.results.reverse().forEach((podcast) => {
+            document.querySelector('.podcast-results').insertAdjacentHTML("afterbegin", `<div class="podcast"><img src="${podcast.artworkUrl60}" alt="d"><li>${podcast.collectionName}</li></div>`);
           });
-      };
+        };
+      });
+  }
+
+  function killEnter() {
+    document.querySelector('.podcast-section form').addEventListener('keypress', function(event) {
+      if (event.keyCode == 13) {
+          event.preventDefault();
+      }
+    });
   }
 }
+
 
 export { executePodcastSearch };
