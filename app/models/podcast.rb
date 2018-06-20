cebff0a4bbed8cadae1d444d7c17bfeba8b69b0f
Ryan Lingle <lingleryan@gmail.com>
2018-06-20 13:39:59 +0200

    alsdk

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,13 +3,16 @@
 class Podcast < ActiveRecord::Base
   has_many :episodes, dependent: :destroy
   has_many :donations, through: :episodes
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   validates_presence_of :name
   validates :name, uniqueness: true
   mount_uploader :artwork, ArtworkUploader
   include PgSearch
+  PgSearch.multisearch_options = { :using => { :tsearch => {:prefix => true, :dictionary => "english"} } }
   pg_search_scope :search,
     against: [ :name ],
     using: {
       tsearch: { prefix: true }
     }
+  multisearchable against: [ :name ]
 end
