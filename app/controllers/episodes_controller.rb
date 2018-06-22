6d3a2d93cef6c16b70b34df2e627d1c591a64899
Ryan Lingle <lingleryan@gmail.com>
2018-06-22 10:16:46 +0200

    some fixes

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,14 +3,19 @@ class EpisodesController < ApplicationController
     if params[:query].present?
       @results = Episode.where(podcast: params[:podcast_id]).search(params[:query])
       respond_to do |format|
-        format.html { redirect_to new_donation_path }
+        format.html { redirect_to podcast_path(params[:podcast_id]) }
         format.js  # <-- will render `app/views/reviews/create.js.erb`
       end
     else
       @results = Episode.all
     end
   end
+
   def index
+    respond_to do |format|
+      format.html { redirect_to podcast_path(params[:podcast_id]) }
+      format.js
+    end
   end
 
   def show
