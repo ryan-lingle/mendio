3a3ef8b42c43b1017b055e387d38adcd11395520
Gabriele Branco <gabi.branco@gmail.com>
2018-06-22 15:14:19 +0200

    rubocop is da best

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@mendio.co'
-  layout 'mailer'
+  default from: "info@mendio.co"
+  layout "mailer"
 end
--- app/mailers/donation_mailer.rb
+++ app/mailers/donation_mailer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DonationMailer < ApplicationMailer
   def creation_confirmation(donation, user)
     @donation = donation
