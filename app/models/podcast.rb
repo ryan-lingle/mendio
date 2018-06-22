bd95372f84bd551663c199189b2db41484da994e
Ryan Lingle <lingleryan@gmail.com>
2018-06-22 17:09:02 +0200

    podcast validation modified

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -5,7 +5,7 @@ class Podcast < ActiveRecord::Base
   has_many :donations, through: :episodes
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   validates_presence_of :name
-  validates :name, uniqueness: true
+  # validates :name, uniqueness: true
   mount_uploader :artwork, ArtworkUploader
   include PgSearch
   PgSearch.multisearch_options = { using: { tsearch: { prefix: true, dictionary: "english" } } }
