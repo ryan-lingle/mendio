4fee456243c20adef6c9ce7171e01d7ec0e31a16
Ryan Lingle <lingleryan@gmail.com>
2018-06-25 17:42:42 +0200

    podcast task added

--- app/jobs/update_podcasts_job.rb
+++ app/jobs/update_podcasts_job.rb
@@ -6,22 +6,23 @@ class UpdatePodcastsJob < ApplicationJob
 
   def perform(podcast_id)
     podcast = Podcast.find(podcast_id)
-    doc = Nokogiri::XML(open(podcast.new_feed))
-    podcast = Podcast.update(
-        name: doc.at('//title').text,
-        description: doc.at('//itunes:summary').text
-      )
-    podcast.remote_artwork_url = doc.at('//itunes:image')['href']
-    podcast.new_feed = doc.at('//itunes:new-feed-url').text if doc.at('//itunes:new-feed-url')
-    podcast.save!
-    doc.xpath('//item').each do |ep|
-      episode = Episode.new(
-        name: ep.xpath('.//title').text,
-        podcast: podcast,
-      )
-      episode.number = ep.at('.//itunes:episode').text if ep.at('.//itunes:episode')
-      episode.description = ep.at('.//itunes:summary').text if ep.at('.//itunes:summary')
-      episode.save!
+    if podcast.new_feed
+      doc = Nokogiri::XML(open(podcast.new_feed))
+      puts "Updating #{doc.at('//title').text}"
+      podcast.name = doc.at('//title').text
+      podcast.description = doc.at('//itunes:summary').text
+      podcast.remote_artwork_url = doc.at('//itunes:image')['href']
+      podcast.new_feed = doc.at('//itunes:new-feed-url').text if doc.at('//itunes:new-feed-url')
+      podcast.save!
+      doc.xpath('//item').each do |ep|
+        episode = Episode.new(
+          name: ep.xpath('.//title').text,
+          podcast: podcast,
+        )
+        episode.number = ep.at('.//itunes:episode').text if ep.at('.//itunes:episode')
+        episode.description = ep.at('.//itunes:summary').text if ep.at('.//itunes:summary')
+        episode.save!
+      end
     end
   end
 end
